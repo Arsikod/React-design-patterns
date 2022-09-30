28be7614c6ba8e08d4aa389405a7caa60b95e356
Arsikod <47626872+Arsikod@users.noreply.github.com>
2022-09-30 21:00:10 +0600

    2 files modified, 2 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import NumberedList from './ListItem/NumberedList';
 import LargePersonListItem from './ListItem/people/LargePersonListItem';
 import { people } from './ListItem/people/mocks/data';
 import SmallPersonListItem from './ListItem/people/SmallPersonListItem';
 import RegularList from './ListItem/RegularList';
+import Modal from './Modal/Modal';
 import SplitScreen from './SplitScreen';
 
 function App() {
   return (
-    <SplitScreen leftWeight={1} rightWeight={1}>
-      <RegularList items={people} component={SmallPersonListItem} />
-
-      <RegularList items={people} component={LargePersonListItem} />
-    </SplitScreen>
+    <>
+      <SplitScreen leftWeight={1} rightWeight={1}>
+        <Modal>
+          <RegularList items={people} component={SmallPersonListItem} />
+        </Modal>
+        <NumberedList items={people} component={LargePersonListItem} />
+      </SplitScreen>
+    </>
   );
 }
 
---
+++ src/ListItem/NumberedList.tsx
@@ -0,0 +1,22 @@
+import { ReactElement, ReactNode } from 'react';
+
+interface INumberedList<T> {
+  items: Array<T>;
+  component: React.ElementType;
+}
+
+export default function NumberedList<T>({
+  items,
+  component: Component,
+}: INumberedList<T>) {
+  return (
+    <>
+      {items.map((item, index) => (
+        <>
+          <h3>{index + 1}</h3>
+          <Component key={index} {...item} />
+        </>
+      ))}
+    </>
+  );
+}
